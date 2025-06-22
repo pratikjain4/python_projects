9845100c2487fe8e9d2d51cd3967f379e8ea85e3
Pratik Jain <pratikjain.pj.4@gmail.com>
2025-06-22 11:04:48 +0530

    Fix 403 error

--- EverythingUpdateChecker/UpdateCheck.py
+++ EverythingUpdateChecker/UpdateCheck.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+from urllib.request import Request, urlopen
+
 import requests
 from bs4 import BeautifulSoup
 import os.path
@@ -9,7 +12,7 @@ import sys
 
 def create_directory():
     try:
-        os.mkdir('downloaded_files/')
+        os.mkdir("downloaded_files/")
     except OSError:
         # in case directory already exists, nothing to do !
         pass
@@ -17,28 +20,32 @@ def create_directory():
 
 def retrieve_html_content():
     headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        # 'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600",
+        "User-Agent": "Mozilla/5.0",
     }
 
-    base_url = 'https://www.voidtools.com'
+    base_url = "https://www.voidtools.com"
     page_url = base_url + "/forum/viewtopic.php?t=9787"
 
     try:
-        req = requests.get(page_url, headers)
+        # req = requests.get(page_url, headers)
+        req = Request(page_url, headers=headers, method="GET")
+        page_contents = urlopen(req).read()
+        # pprint(page_contents)
     except requests.ConnectionError:
         error_msg = "Error connecting to Everything website over the internet ! "
         print(error_msg)
         ctypes.windll.user32.MessageBoxW(0, error_msg, "Everything", 0)
         sys.exit(1)
 
-    soup = BeautifulSoup(req.content, 'html.parser')
-
-    all_links = soup.find_all('a', href=True)
-    download_links = [url['href'] for url in all_links if 'exe' in url['href']]
+    soup = BeautifulSoup(page_contents, "html.parser")
+    # pprint(soup)
+    all_links = soup.find_all("a", href=True)
+    # pprint(all_links)
+    download_links = [url["href"] for url in all_links if "exe" in url["href"]]
     if not download_links:
         print("Not able to access the Everything Alpha update forums page ! ")
         sys.exit(1)
@@ -49,16 +56,20 @@ def retrieve_html_content():
 
 def check_and_download_file(exe_link):
     print("Latest build : " + exe_link)
-    filename = exe_link.split('/')[-1]
-    file_exists = os.path.isfile('downloaded_files/' + filename)
+    filename = exe_link.split("/")[-1]
+    file_exists = os.path.isfile("downloaded_files/" + filename)
 
     if file_exists:
-        ctypes.windll.user32.MessageBoxW(0, "Everything is up to date!", "Everything", 0)
-        subprocess.Popen("explorer /select," + 'downloaded_files\\' + filename)
+        ctypes.windll.user32.MessageBoxW(
+            0, "Everything is up to date!", "Everything", 0
+        )
+        subprocess.Popen("explorer /select," + "downloaded_files\\" + filename)
     else:
-        ctypes.windll.user32.MessageBoxW(0, "New version of Everything found!  Downloading....!", "Everything", 0)
-        urllib.request.urlretrieve(exe_link, 'downloaded_files/' + filename)
-        subprocess.Popen("explorer /select," + 'downloaded_files\\' + filename)
+        ctypes.windll.user32.MessageBoxW(
+            0, "New version of Everything found!  Downloading....!", "Everything", 0
+        )
+        urllib.request.urlretrieve(exe_link, "downloaded_files/" + filename)
+        subprocess.Popen("explorer /select," + "downloaded_files\\" + filename)
 
     # Display Latest file
     print("Latest file   : " + filename)
